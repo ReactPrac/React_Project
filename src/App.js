dab8d487ed66a11241f169e348e6b1b4bcbc21da
silverywaves <babyshu1210@gmail.com>
2024-07-09 19:53:45 +0900

    state

--- src/App.js
+++ src/App.js
@@ -1,15 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
 	let post = '리액트 기본 문법';
+	let [title, b] = useState([
+		'리액트 데이터바인딩',
+		'리액트 연습',
+		'집에 가고싶다',
+	]);
 
 	return (
 		<div className="App">
 			<div className="black-nav">
-				<h4 style={{ color: 'yellow', fontSize: '18px' }}>블로그</h4>
+				<h4 style={{ fontSize: '18px' }}>ReactBlog</h4>
 			</div>
-			<h4>{post}</h4>
+			<div className="list">
+				<h4>글 제목</h4>
+				<p>7월 9일 발행</p>
+			</div>
+			<div className="list">
+				<h4>{title[0]}</h4>
+				<p>7월 9일 발행</p>
+			</div>
+			<div className="list">
+				<h4>{post}</h4>
+				<p>7월 9일 발행</p>
+			</div>
+			<div className="list">
+				<h4>{title[1]}</h4>
+				<p>7월 9일 발행</p>
+			</div>
+			<div className="list">
+				<h4>{title[2]}</h4>
+				<p>7월 9일 발행</p>
+			</div>
 		</div>
 	);
 }
